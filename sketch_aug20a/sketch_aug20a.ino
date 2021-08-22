7e1d7cdbdcbf1c91709c16c8e7ad61344d6582b8
Andy(Pengan) Zhou <pengan1987@msn.com>
2021-08-22 22:32:33 +0800

    Put robot arm and stepper toghter

--- sketch_aug20a/sketch_aug20a.ino
+++ sketch_aug20a/sketch_aug20a.ino
@@ -1,47 +1,124 @@
-#include <Servo.h> 
+#include <Servo.h>
+
+int stepPin = 12;
+int dirPin = 11;
+int enblPin = 10;
+int armPin = 9;
+char armStatus;
 
 Servo myservo;
-int leftEnd =170;
-int rightEnd=130;
+int leftEnd = 170;
+int rightEnd = 130;
+String digitBuffer = "";
 
-void setup() 
-{ 
-    Serial.begin(9600);
+void setup()
+{
+  //Init serial
+  Serial.begin(9600);
 
-  myservo.attach(9);
-  myservo.write(300);  // set servo to mid-point
-    Serial.println("\n\nString toInt():");
+  //Init servo
+  myservo.attach(armPin);
+  myservo.write(leftEnd); // set servo to left-point
+  armStatus = 'l';
 
-} 
+  //Init stepper
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(enblPin, OUTPUT);
+  digitalWrite(stepPin, LOW);
+  digitalWrite(dirPin, LOW);
+  digitalWrite(enblPin, LOW);
 
-String inString = ""; 
-void loop() {
+  Serial.println("\n\nKnitting Machine Ready:");
+  Serial.print("\nCommand>");
+}
+
+void loop()
+{
+
   // Read serial input:
-  while (Serial.available() > 0) {
+  while (Serial.available() > 0)
+  {
     int inChar = Serial.read();
-    if (isDigit(inChar)) {
-      // convert the incoming byte to a char
-      // and add it to the string:
-      inString += (char)inChar;
+    long commandTimes = 1;
+    if (digitBuffer.length() > 0)
+    {
+      commandTimes = digitBuffer.toInt();
     }
-    // if you get a newline, print the string,
-    // then the string's value:
-    if (inChar == '\n') {
-      Serial.print("Value:");
-      Serial.println(inString.toInt());
-      Serial.print("String: ");
-      Serial.println(inString);
-
-      for(int x=0;x<inString.toInt();x++){
-        myservo.write(leftEnd);
-        delay(1000);
-                myservo.write(rightEnd);
-        delay(1000);
+    if (inChar == 'z')
+    {
+      armStatus = 'l';
+      Serial.println("arm to zero - for fix");
+      myservo.write(0);
+    }
+    if (inChar == 'l')
+    {
+      armStatus = 'l';
+      Serial.println("arm to left - loose");
+      myservo.write(leftEnd);
+    }
+    else if (inChar == 'r')
+    {
+      armStatus = 'r';
+      Serial.println("arm to right - tight");
+      myservo.write(rightEnd);
+    }
+    else if (inChar == 's')
+    {
+      if (armStatus == 'l' && commandTimes > 5)
+      {
+        commandTimes = 5;
+        Serial.println("Warning: you can't loose more than 5 steps");
       }
-
-   
-      // clear the string for new input:
-      inString = "";
+      Serial.println("wheel forward " + digitBuffer + " step");
+      rollHandle(256000L / 48L * commandTimes);
+    }
+    else if (inChar == 'b')
+    {
+      Serial.println("wheel back " + digitBuffer + " step");
+      digitalWrite(dirPin, HIGH);
+      delay(100);
+      rollHandle(256000L / 48L * commandTimes);
+      digitalWrite(dirPin, LOW);
+      delay(100);
+    }
+    else if (inChar == 'c')
+    {
+      if (armStatus == 'r')
+      {
+        Serial.println("wheel forward " + digitBuffer + " circle");
+        rollHandle(256000L * commandTimes);
+      }
+      else
+      {
+        Serial.println("Warning: you can't loose more than 5 steps");
+      }
+    }
+    if (isDigit(inChar))
+    {
+      //Add new digit into digit buffer
+      digitBuffer += (char)inChar;
+    }
+    else
+    {
+      //After machinecal action, wait for 1sec, clean digit buffer
+      digitBuffer = "";
+      delay(500);
+      Serial.print("\nCommand>");
     }
   }
+}
+
+void rollHandle(long steps)
+{
+  long i;
+  Serial.println("roll handle for steps: " + String(steps));
+  for (i = 0; i < steps; i++)
+  {
+    digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(20);
+  }
+
+  Serial.println("rolling done");
 }
